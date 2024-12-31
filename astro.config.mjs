1e7314245731cfc577ef99ea12c4135670ebfd93
Daniel <deumolo@gmail.com>
2024-12-30 18:26:05 -0600

    feat(dependencies): install netlify adapter package
    Added the netlify package to the project for deployment functionality.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,10 @@ import db from '@astrojs/db';
 
 import tailwind from '@astrojs/tailwind';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db(), tailwind()]
+  integrations: [db(), tailwind()],
+  adapter: netlify()
 });
