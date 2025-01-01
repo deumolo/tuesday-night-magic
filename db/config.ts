0e0e1a7989b53151d023b2e959cfdb09ef38e3ad
Daniel <deumolo@gmail.com>
2025-01-01 17:30:38 -0600

    feat(auth): add user registration functionality
    Implemented the user registration feature. This includes:
    
    - Created a server action to handle user registration requests.
    - Defined the necessary 'libsql' tables for storing user data.
    - Built a 'register.astro' page for the user registration interface.

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,72 @@
-import { defineDb } from 'astro:db';
+import { defineDb, defineTable, column } from "astro:db";
 
-// https://astro.build/db/config
+const User = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true, unique: true }),
+    name: column.text(),
+    email: column.text({ unique: true }),
+    password: column.text(),
+    createdAt: column.date({ default: new Date() }),
+  },
+});
+
+const Group = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true, unique: true }),
+    name: column.text(),
+    public: column.boolean({ default: true }),
+    password: column.text({ optional: true }),
+    createdAt: column.date({ default: new Date() }),
+  },
+});
+
+const Deck = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true, unique: true }),
+    name: column.text(),
+    moxfieldLink: column.text(),
+    player: column.text({ references: () => User.columns.id }),
+    createdAt: column.date({ default: new Date() }),
+  },
+});
+
+const Match = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true, unique: true }),
+    group: column.text({ references: () => Group.columns.id }),
+    winner: column.text({ references: () => User.columns.id }),
+    createdAt: column.date({ default: new Date() }),
+  },
+});
+
+const UserGroup = defineTable({
+  columns: {
+    userId: column.text({ references: () => User.columns.id }),
+    groupId: column.text({ references: () => Group.columns.id }),
+    role: column.text({ optional: true }),
+    createdAt: column.date({ default: new Date() }),
+  },
+});
+
+const MatchPlayerStats = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true, unique: true }),
+    matchId: column.text({ references: () => Match.columns.id }),
+    playerId: column.text({ references: () => User.columns.id }),
+    opponentId: column.text({ references: () => User.columns.id }),
+    killed: column.boolean({ default: false }),
+    killedWithCommanderDamage: column.boolean({ default: false }),
+    createdAt: column.date({ default: new Date() }),
+  },
+});
+
 export default defineDb({
-  tables: {}
+  tables: {
+    User,
+    Group,
+    Deck,
+    Match,
+    UserGroup,
+    MatchPlayerStats,
+  },
 });
---
+++ src/actions/auth/register.action.ts
@@ -0,0 +1,64 @@
+import { LibsqlError } from "@libsql/client";
+import { ActionInputError } from "astro:actions";
+import { defineAction } from "astro:actions";
+import { z } from "astro:schema";
+import { db, User } from "astro:db";
+import bcrypt from "bcryptjs";
+import { v4 as UUID } from "uuid";
+
+export const registerUser = defineAction({
+  accept: "form",
+
+  input: z.object({
+    name: z.string().min(2),
+    email: z.string().email(),
+    password: z.string().min(6),
+    confirmPassword: z.string().min(6),
+  }),
+
+  handler: async ({ name, email, password, confirmPassword }, { cookies }) => {
+    try {
+      if (password !== confirmPassword) {
+        return {
+          success: false,
+          error: "Password and confirm password do not match",
+        };
+      }
+
+      const newUser = {
+        id: UUID(),
+        name,
+        email,
+        password: bcrypt.hashSync("1234"),
+      };
+
+      await db.insert(User).values(newUser);
+
+      return {
+        success: true,
+        data: newUser,
+      };
+    } catch (error) {
+      if (error instanceof ActionInputError) {
+        return {
+          success: false,
+          error: error.issues.map((issue) => issue.message).join("\n"),
+        };
+      }
+
+      if (
+        error instanceof LibsqlError &&
+        (error as Error).message.includes(
+          "UNIQUE constraint failed: User.email"
+        )
+      ) {
+        return {
+          success: false,
+          error: "Email already exists. Please use a different email address.",
+        };
+      }
+
+      return { success: false, error: JSON.stringify(error) };
+    }
+  },
+});
