c866a4b59c744be72997f1f436b6868c2d3f1fa5
Daniel <deumolo@gmail.com>
2025-10-24 17:45:24 -0600

    Made changes for different adapters to different environments

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,18 @@
 import { defineConfig } from 'astro/config';
 import db from '@astrojs/db';
 import tailwind from '@astrojs/tailwind';
+import node from '@astrojs/node';
 import netlify from '@astrojs/netlify';
 import auth from 'auth-astro';
 import icon from 'astro-icon';
 
+// Use different adapters for development vs production
+const isDev = process.env.NODE_ENV === 'development' || !process.env.NETLIFY;
+const adapter = isDev ? node({ mode: 'standalone' }) : netlify();
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [db(), tailwind(), auth(), icon()],
-  adapter: netlify(
-    {
-      edgeMiddleware: false,
-    }
-  ),
+  adapter: adapter,
   output: 'server',
 });
