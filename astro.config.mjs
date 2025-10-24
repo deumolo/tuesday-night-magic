5fc8a6f43e65e8ab7b13ada42ac648b498a11af5
Daniel <deumolo@gmail.com>
2025-10-24 12:59:38 -0600

    Keep trying

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,13 +11,9 @@ import auth from 'auth-astro';
 
 import icon from 'astro-icon';
 
-import vercel from '@astrojs/vercel';
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [db(), tailwind(), auth(), icon()],
-  adapter: netlify({
-    edgeMiddleware: false,
-  }),
+  adapter: netlify(),
   output: 'server',
 });
